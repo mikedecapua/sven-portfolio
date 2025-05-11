7c7942b35f86218412521506ede8e977c39ab7c9
Michael DeCapua <michaeldecapua@macbookair.mynetworksettings.com>
2025-05-11 19:50:41 -0400

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
